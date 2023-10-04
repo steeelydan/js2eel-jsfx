7cc0e6002d3dc61d129d10e128133a8d20082032
Steeely Dan <steeelydan@posteo.de>
2023-10-04 09:05:58 +0200

    Volume; add remaining compiled jsfx

---
+++ js-src/sd_volume_2.js
@@ -0,0 +1,20 @@
+config({ description: 'sd_volume_2', inChannels: 2, outChannels: 2 });
+
+let volume = 0;
+let target = 0;
+
+slider(1, volume, 0, -150, 18, 0.1, 'Volume [dB]');
+
+onSlider(() => {
+    if (volume > -149.9) {
+        target = 10 ** (volume / 20);
+    } else {
+        target = 0;
+    }
+});
+
+onSample(() => {
+    eachChannel((sample, _ch) => {
+        sample *= target;
+    });
+});
